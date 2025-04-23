9696b151c1d9f3c89fbc11913041aacd392daf4b
lucky44x <nickbali06@gmail.com>
2025-04-23 15:38:38 +0200

    Added Support for Versions 1.21 - 1.21.4 (Fuck 1.20.5 and 1.20.6 that shit DOES NOT build on my local machine)
    Additionally, I have no Idea if my implementation actually works, since Mojang decided they wanted to randomly do a complete makeover of how they handle Items

---
+++ luckyutil_1_21_R3/src/main/java/com/github/lucky44x/luckyutil/chat/versions/NMSChatProvider_1_21_R3.java
@@ -0,0 +1,28 @@
+package com.github.lucky44x.luckyutil.chat.versions;
+
+
+import com.github.lucky44x.luckyutil.chat.abstraction.NMSChatProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_21_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_21_R3.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+public class NMSChatProvider_1_21_R3 extends NMSChatProvider {
+    @Override
+    protected String convertItemToChatString(ItemStack originalItem) {
+        NBTTagCompound compound = new NBTTagCompound();
+        MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer(); // Cursed but whatever
+        HolderLookup.a lookup = server.ba();
+
+        net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(originalItem);
+
+        // IChatBaseComponent component = nmsItem.F();
+        // return IChatBaseComponent.ChatSerializer.a(component, lookup);
+
+        compound = (NBTTagCompound) nmsItem.b(lookup, compound);
+        return compound.toString();
+    }
+}
